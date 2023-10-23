0708c38f561f1c30d2542d7a14b5b7cdc8f94609
apaixav <lucasrib01@gmail.com>
2023-10-23 14:29:52 -0300

    Home

---
+++ BlogPessoal/src/pages/Home/home.tsx
@@ -0,0 +1,43 @@
+
+import './home.css'
+
+function Home() {
+  return (
+  <body>
+      <>
+          <div style={{
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center"
+          }}>
+              <div>
+                  <div style={{
+                       width: "80vw",
+                       display: "flex",
+                       flexDirection: "column",
+                       alignItems: "center",                                         
+                  }}>
+                      <h2>Seja Bem Vinde!</h2>
+                      <p>Expresse aqui seus pensamentos e opniões!</p>
+                  </div>
+
+                  <div style={{
+                       width: "80vw",
+                       display: "flex",
+                       flexDirection: "column",
+                       alignItems: "center"
+                  }}>
+                      <img 
+                          src="https://i.imgur.com/VpwApCU.png" 
+                          alt="Imagem da Página Home" 
+                          width="400px"
+                      />
+                  </div>
+              </div>
+          </div>
+      </>
+</body>
+  )
+}
+
+export default Home
